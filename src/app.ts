3465f31b6f4830d7bffeb767a9b2688377e9e878
Paras <paras.jain2@publicissapient.com>
2025-11-01 11:12:03 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Response } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import { router as adminRoutes } from "./routes/admin";
