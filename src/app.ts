e0be772a1ac8078533a4b1c507d00347df92f347
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-05 20:45:24 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express, { Response } from "express";
-import bodyParser from "body-parser";
 import path from "path";
 import { router as adminRoutes } from "./routes/admin";
 import { router as shopRoutes } from "./routes/shop";
 import { viewsPath } from "./util/path";
 
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(shopRoutes);
 app.use("/admin", adminRoutes);
