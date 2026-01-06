739f34c55dde67bd2a963e6ad4bd4eff065e3a00
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-06 15:52:20 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
-import express, { Response } from "express";
-import bodyParser from "body-parser";
+import express, {
+  static as express_static,
+  urlencoded,
+  Response,
+} from "express";
 import path from "path";
 import { router as adminRoutes } from "./routes/admin";
 import { router as shopRoutes } from "./routes/shop";
-import { viewsPath } from "./util/path";
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(urlencoded({ extended: false }));
+app.use(express_static(path.join(__dirname, "public")));
 
 app.use(shopRoutes);
 app.use("/admin", adminRoutes);
 
-app.use((_, res:Response) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path:'/' });
+app.use((_, res: Response) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/" });
 });
 
 app.listen(3000);
