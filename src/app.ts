88495ea204a8e4f35660e2861c1278148ed0a1aa
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-06 18:30:13 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
-import express, { Response } from "express";
+import express, { static as express_static, Response } from "express";
 import bodyParser from "body-parser";
-import path from "path";
+import { join } from "path";
 import { router as adminRoutes } from "./routes/admin";
 import { router as shopRoutes } from "./routes/shop";
 
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express_static(join(__dirname, "public")));
 
 app.use(shopRoutes);
 app.use("/admin", adminRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,7 +1,7 @@
-import express, { Response } from "express";
+import { Router, Response } from "express";
 import { products } from "./admin";
 
-export const router = express.Router();
+export const router = Router();
 
 router.get("/", (_, res: Response) => {
   res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
