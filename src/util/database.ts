73be9687140d7c6b74308d7d0f157d35aced2977
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-07 17:32:38 +0530

    code updated

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,6 +1,14 @@
+import { config } from "dotenv";
+config(); // MUST be first
+
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("node-complete", "root", "gulshan1006", {
-  dialect: "mysql",
-  host: "localhost",
-});
+export const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE as string,
+  process.env.MYSQL_USERNAME as string,
+  process.env.MYSQL_PASSWORD as string,
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
