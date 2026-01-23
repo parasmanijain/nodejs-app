7c365f6a988a85d552b55ef2c37240e6722b8265
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-01-23 21:27:46 +0530

    code updated

--- src/controllers/shop.ts
+++ src/controllers/shop.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { join } from "path";
 import { Types } from "mongoose";
-import { createReadStream } from "fs";
+import { createWriteStream } from "fs";
 import PDFDocument from "pdfkit";
 import { HttpError } from "../types/http-error";
 import { Product } from "../models/product";
@@ -234,13 +234,36 @@ export const getInvoice = async (
     }
     const invoiceName = "invoice-" + orderId + ".pdf";
     const invoicePath = join("data", "invoices", invoiceName);
-    const file = createReadStream(invoicePath);
+    const pdfDoc = new PDFDocument();
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader(
       "Content-Disposition",
       'inline; filename="' + invoiceName + '"',
     );
-    file.pipe(res);
+    pdfDoc.pipe(createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+
+    pdfDoc.fontSize(26).text("Invoice", {
+      underline: true,
+    });
+    pdfDoc.text("-----------------------");
+    let totalPrice = 0;
+    order.products.forEach((prod) => {
+      totalPrice += prod.quantity * prod.product.price;
+      pdfDoc
+        .fontSize(14)
+        .text(
+          prod.product.title +
+            " - " +
+            prod.quantity +
+            " x " +
+            "$" +
+            prod.product.price,
+        );
+    });
+    pdfDoc.text("---");
+    pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
+    pdfDoc.end();
   } catch (err) {
     next(err);
   }
